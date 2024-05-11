9c4cbfdf6626bd2814e611f03bb65f8a1029def4
SunnyChavan7 <sunnychavan956@gmail.com>
2024-05-11 20:22:31 +0530

    variables in JS

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,3 +1,19 @@
-// variables
-console.log("Helllo World")
-console.log("Helllo World")
+const accountId = 1444553
+let accountEmail = "sunny21comp@student.mes.ac.in"
+var accountPassword = "Sunny"
+accountCity = "Navi Mumbai"
+let accountState;   // undefined
+
+// accountId = 2  // not allowed
+// console.log(accountId)
+
+accountEmail = "sunny@123"
+accountPassword = "HelloWorld!"
+accountCity = "Sindhudurg"
+
+/*
+Prefer not to use var
+because of issue of block scope and functional scope
+*/
+
+console.table([accountEmail, accountPassword, accountCity, accountId, accountState]);
---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,15 @@
+// "use strict";  // treat all JS code as newer version
+
+// alert(3+3)  // we are using node js and not browser
+
+// official documentation :  ECMAScript
+// mdn 
+let name = "sunny"
+let age = 21
+let isLoggedIn = false
+// null => stand alone value
+// undefined => value is not assigned
+// symbol => unique
+
+// object 
+console.log(typeof "sunny");
